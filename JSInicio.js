f9e30cb9e815a0427e5d69792b0c070f6e4b0042
Eve <culinevelio@gmail.com>
2024-03-04 08:34:29 -0600

    Mi primer JS

---
+++ JSInicio.js
@@ -0,0 +1,15 @@
+let v3= 3;
+
+function s(v1, v2){
+    return v1+v2+v3
+}
+
+console.log(s(1,2))
+
+let mensaje = function mostrarmensaje(text){
+    console.log(text)
+};
+
+mensaje('Hola');
+console.log(mensaje);
+console.log(typeof mensaje)
